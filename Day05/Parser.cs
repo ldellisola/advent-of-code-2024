a8909b092d41601e68427357b772e8b64a32b523
Lucas Dell'Isola <lucas.dellisola@rystadenergy.com>
2024-12-05 10:59:14 +0100

    Day 5

---
+++ Day05/Parser.cs
@@ -0,0 +1,43 @@
+﻿namespace Day05;
+
+public static class Parser
+{
+    public static IEnumerable<(int,int)> LoadOrderRules(StreamReader reader)
+    {
+        do
+        {
+            var line = reader.ReadLine();
+            if (string.IsNullOrWhiteSpace(line))
+                yield break;
+            var span = line.AsSpan();
+            var divider = span.IndexOf('|');
+            
+            yield return (int.Parse(span[..divider]), int.Parse(span[(divider + 1)..]));
+        }while (!reader.EndOfStream);
+    }
+
+    public static Dictionary<int, HashSet<int>> ToDictionaryWithRepeated(this IEnumerable<(int, int)> enumerable)
+    {
+        var dictionary = new Dictionary<int, HashSet<int>>();
+
+        foreach(var (first, second) in enumerable)
+        {
+            if (!dictionary.TryGetValue(second, out var value))
+                dictionary.Add(second, [first]);
+            else
+                value.Add(first);
+        }
+
+        return dictionary;
+    }
+
+    public static IEnumerable<int[]> LoadSafetyManuals(StreamReader reader)
+    {
+        do
+        {
+            var line = reader.ReadLine();
+            yield return line!.Split(',').Select(int.Parse).ToArray();
+        }
+        while (!reader.EndOfStream);
+    }
+}
---
+++ Day05/Program.cs
@@ -0,0 +1,112 @@
+﻿using static Day05.Parser;
+
+using var file = File.OpenRead("input.txt");
+using var streamReader = new StreamReader(file);
+
+var rules = LoadOrderRules(streamReader).ToDictionaryWithRepeated();
+var manuals = LoadSafetyManuals(streamReader).ToArray();
+
+
+// ----------------------------------
+
+List<int[]> validManuals = [];
+foreach(var manual in manuals)
+{
+    var localRules = rules
+                     .Where(t=> manual.Contains(t.Key))
+                     .ToDictionary(t=> t.Key, t => t.Value.Where(r=> manual.Contains(r)).ToHashSet());
+    HashSet<int> pagesFound = [];
+    var isValid = true;
+    foreach(var page in manual)
+    {
+        if(!localRules.TryGetValue(page, out var previousPages))
+        {
+            pagesFound.Add(page);
+            continue;
+        }
+
+        if(previousPages.IsSubsetOf(pagesFound))
+        {
+            pagesFound.Add(page);
+            continue;
+        }
+        
+        isValid = false;
+        break;
+    }
+    
+    if (isValid)
+        validManuals.Add(manual);
+}
+
+var result = validManuals.Sum(t=> t[ t.Length /2 + (t.Length % 2 == 0 ? 1 : 0) ] );
+Console.WriteLine("Challenge 1:");
+Console.WriteLine(result);
+// ---------------------------------------------------------------------------------------
+
+List<List<int>> invalidManuals = [];
+// Finding invalid manuals
+foreach(var manual in manuals)
+{
+    var localRules = rules
+                     .Where(t=> manual.Contains(t.Key))
+                     .ToDictionary(t=> t.Key, t => t.Value.Where(r=> manual.Contains(r)).ToHashSet());
+    HashSet<int> pagesFound = [];
+    var isValid = true;
+    foreach(var page in manual)
+    {
+        if(!localRules.TryGetValue(page, out var previousPages))
+        {
+            pagesFound.Add(page);
+            continue;
+        }
+
+        if(previousPages.IsSubsetOf(pagesFound))
+        {
+            pagesFound.Add(page);
+            continue;
+        }
+        
+        isValid = false;
+    }
+    
+    if (!isValid)
+        invalidManuals.Add(manual.ToList());
+}
+
+// fix invalid manuals
+
+List<List<int>> fixedManuals = [];
+foreach(var manual in invalidManuals)
+{
+    var localRules = rules
+                     .Where(t=> manual.Contains(t.Key))
+                     .SelectMany(t=>t.Value.Where(r=> manual.Contains(r)).Select(r=> (t.Key, r)))
+                     .ToHashSet();
+
+    var fixedManual = manual.ToList();
+    bool hasChanged = false;
+    do
+    {
+        hasChanged = false;
+        foreach(var (after, before) in localRules)
+        {
+            var iAfter = fixedManual.IndexOf(after);
+            var iBefore = fixedManual.IndexOf(before);
+
+            if(iAfter == -1 || iBefore == -1 || iBefore <= iAfter)
+            {
+                continue;
+            }
+            hasChanged = true;
+            fixedManual[iAfter] = before;
+            fixedManual[iBefore] = after;
+        }
+    }while(hasChanged);
+
+    fixedManuals.Add(fixedManual);
+}
+
+result = fixedManuals.Sum(t=> t[ t.Count /2 + (t.Count % 2 == 0 ? 1 : 0) ] );
+Console.WriteLine("Challenge 2:");
+Console.WriteLine(result);
