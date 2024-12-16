626f05bdec96957b4b4bcf9fcbb1e8c2dfde69bc
Lucas Dell'Isola <dev@lucasdellisola.com.ar>
2024-12-16 08:38:22 +0100

    WIP day 16

---
+++ Common/Vector.cs
@@ -0,0 +1,73 @@
+﻿using System.Numerics;
+
+namespace Common;
+
+public class Vector(int row, int col) : IAdditionOperators<Vector,Vector,Vector>
+{ 
+    public static readonly Vector Up = new(row: -1, col: 0);
+    public static readonly Vector Down = new(row: 1, col: 0);
+    public static readonly Vector Left = new(row: 0, col: -1);
+    public static readonly Vector Right = new(row: 0, col: 1);
+
+    public int Row => row;
+    public int Col => col;
+    
+    public static Vector operator +(Vector left, Vector right)
+    {
+        return new Vector(left.Row + right.Row, left.Col + right.Col);
+    }
+
+    public Vector RotateLeft()
+    {
+        return this switch
+        {
+            (-1, 0) => Left,
+            (1, 0) => Right,
+            (0, -1) => Down,
+            (0, 1) => Up,
+            _ => throw new InvalidOperationException("Only scalars are supported")
+        };
+    }
+    public Vector RotateRight()
+    {
+        return this switch
+        {
+            (-1, 0) => Right,
+            (1, 0) => Left,
+            (0, -1) => Up,
+            (0, 1) => Down,
+            _ => throw new InvalidOperationException("Only scalars are supported")
+        };    }
+
+    public void Deconstruct(out int row, out int col)
+    {
+        row = Row;
+        col = Col;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not Vector v)
+            return false;
+        return Row == v.Row && Col == v.Col;
+    }
+
+    public static bool operator ==(Vector v1, Vector v2)
+    {
+        return v1.Equals(v2);
+    }
+    public static bool operator !=(Vector v1, Vector v2)
+    {
+        return !v1.Equals(v2);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Row, Col);
+    }
+
+    public override string ToString()
+    {
+        return $"({Row}, {Col})";
+    }
+}
---
+++ Day16/Program.cs
@@ -0,0 +1,9 @@
+﻿using Day16.Challenge1;
+
+var input = File.ReadAllLines("input.txt").Select(t=> t.ToCharArray()).ToArray();
+
+var maze = new Maze(input);
+
+var shortestPath = maze.CalculateRun();
+
+Console.WriteLine(shortestPath);
